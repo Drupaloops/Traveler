fbbe9359c8f6724c9aaf02185348e6f2540f32b5
Drupaloops <opduhao@gmail.com>
2016-10-31 22:11:49 +0800

    test

--- routes/tests.js
+++ routes/tests.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	res.render('index', { title: 'Express' });
 });
 router.get('/todo', function(req, res, next) {
-  res.render('todo', { title: 'toDo' });
+	res.render('todo', { title: 'toDo' });
 });
 
 module.exports = router;
